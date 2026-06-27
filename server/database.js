784f3f8e3904fef9c4364e4fec3bdf9ffa2d06d7
muzammilrazamd-svg <muzammilrazamd@gmail.com>
2026-06-27 18:59:05 +0530

    Connected railway mysql

--- server/database.js
+++ server/database.js
@@ -1,15 +1,18 @@
 const mysql = require("mysql2");
 
+
+/*const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});*/
 const connection = mysql.createConnection({
-
-host:"localhost",
-
-user:"root",
-
-password:"Mrraza905",
-
-database:"attendance_system"
-
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 connection.connect((err)=>{
